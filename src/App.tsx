946d4e66827ab290a8ea8e67b0f27214cc45ba23
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-07-23 00:06:13 +0300

    feat(ui): add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 
+import { Route, Routes } from 'react-router-dom'
 import SearchPage from './pages/search-page/search-page'
+import MovieDetailsPage from './pages/search-page/movie-details-pages/movie-details'
 
 function App() {
 
   return (
     <>
-      <SearchPage />
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/movie/:id" element={<MovieDetailsPage />} />
+      </Routes>
     </>
   )
 }
